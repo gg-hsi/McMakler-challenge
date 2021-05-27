a89699964effdea15ac8145fb633edb2ae36922f
gg <7249811+gaaliche@users.noreply.github.com>
2021-05-27 10:41:26 +0200

    last update api test

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -14,10 +14,22 @@ describe('create new ad', () => {
     const adCreated = await createNewAd(apiUrl, adData);
     expect({ ...adData, _id: adCreated._id }).toStrictEqual(adCreated);
   });
-  it('edit existing', async () => {
-    const ads = await getAllAds(apiUrl);
+});
+describe('edit existing ad', () => {
+  let ads = [];
+  const adData = {
+    name: getRandomString(5),
+    street: getRandomString(10),
+    rooms: getRandomNumber(),
+    price: getRandomNumber().toString(),
+    status: true,
+  };
+  it('get ad to be modified', async () => {
+    ads = await getAllAds(apiUrl);
     expect(ads).toBeTruthy();
-    await editAd(apiUrl, ads[0]._id);
+  });
+  it('edit ad', async () => {
+    await editAd(apiUrl, ads[0]._id, adData);
     const adEdited = await getAd(apiUrl, ads[0]._id);
     expect(ads[0]).not.toBe(adEdited);
     expect(ads[0]._id).toStrictEqual(adEdited._id);
--- helpers/api.js
+++ helpers/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { getRandomString, getRandomNumber } from './utils';
 
 export async function getAllAds(url) {
   const config = {
@@ -44,18 +43,11 @@ export async function createNewAd(url, data) {
   return response.data;
 }
 
-export async function editAd(url, adID) {
+export async function editAd(url, adID, data) {
   const config = {
     url: `${url}/${adID}`,
     method: 'put',
-    data: {
-      _id: adID,
-      name: getRandomString(5),
-      street: getRandomString(10),
-      rooms: getRandomNumber(),
-      price: getRandomNumber().toString(),
-      status: true,
-    },
+    data: { ...data, _id: adID },
     timeout: 2000,
     headers: { 'Content-Type': 'application/json' },
     validateStatus: function (status) {
